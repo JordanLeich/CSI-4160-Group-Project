d5aafe53ff6b56ee49182aba18ca452ce71320c4
Jordan Leich <jordanleich@gmail.com>
2024-10-10 10:17:17 -0400

    Testing

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,16 @@
+from flask import Flask, render_template, request, jsonify
 from sense_hat import SenseHat
 from time import sleep
 
+app = Flask(__name__)
 sense = SenseHat()
+
 X_color = [255, 0, 0]    # Red for X player
 O_color = [0, 0, 255]    # Blue for O player
 empty_color = [0, 0, 0]  # Black
 select_color = [0, 255, 0]  # Green
 
-board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-]
-
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 current_player = 'X'
 cursor = [0, 0]
 
@@ -33,24 +31,6 @@ def display_board():
         pixels.extend([empty_color] * 8)
     sense.set_pixels(pixels)
 
-def move_cursor(direction):
-    global cursor
-    if direction == 'up':
-        cursor[0] = (cursor[0] - 1) % 3
-    elif direction == 'down':
-        cursor[0] = (cursor[0] + 1) % 3
-    elif direction == 'left':
-        cursor[1] = (cursor[1] - 1) % 3
-    elif direction == 'right':
-        cursor[1] = (cursor[1] + 1) % 3
-
-def place_marker():
-    global current_player
-    row, col = cursor
-    if board[row][col] == ' ':
-        board[row][col] = current_player
-        current_player = 'O' if current_player == 'X' else 'X'
-
 def check_winner():
     for row in board:
         if row[0] == row[1] == row[2] and row[0] != ' ':
@@ -66,21 +46,49 @@ def check_winner():
         return 'Tie'
     return None
 
-while True:
+def place_marker():
+    global current_player
+    row, col = cursor
+    if board[row][col] == ' ':
+        board[row][col] = current_player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+# Web routes
+@app.route('/')
+def index():
+    return render_template('index.html', board=board, current_player=current_player)
+
+@app.route('/move', methods=['POST'])
+def move():
+    direction = request.json.get('direction')
+    if direction:
+        move_cursor(direction)
+        display_board()
+    return jsonify({"status": "success"})
+
+@app.route('/place_marker', methods=['POST'])
+def place_marker_route():
+    place_marker()
+    winner = check_winner()
     display_board()
-    sleep(0.1)
-    for event in sense.stick.get_events():
-        if event.action == 'pressed':
-            if event.direction in ['up', 'down', 'left', 'right']:
-                move_cursor(event.direction)
-            elif event.direction == 'middle':
-                place_marker()
-                winner = check_winner()
-                if winner:
-                    if winner != 'Tie':
-                        sense.show_message("{} wins!".format(winner), scroll_speed=0.05)
-                    else:
-                        sense.show_message("It's a tie!", scroll_speed=0.05)
-                    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-                    current_player = 'X'
-                    cursor = [0, 0]
+    if winner:
+        if winner != 'Tie':
+            message = "{} wins!".format(winner)
+        else:
+            message = "It's a tie!"
+        return jsonify({"status": "game_over", "message": message})
+    return jsonify({"status": "success"})
+
+def move_cursor(direction):
+    global cursor
+    if direction == 'up':
+        cursor[0] = (cursor[0] - 1) % 3
+    elif direction == 'down':
+        cursor[0] = (cursor[0] + 1) % 3
+    elif direction == 'left':
+        cursor[1] = (cursor[1] - 1) % 3
+    elif direction == 'right':
+        cursor[1] = (cursor[1] + 1) % 3
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
